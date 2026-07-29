e8beaacd65f25c23322a857c458cd5459b1b051e
JailsonDev2005 <devjailson43@gmail.com>
2026-07-28 23:31:28 -0300

    aula 3 concluida

---
+++ Curso_Javascript/aulas_js/aula003/script.js
@@ -0,0 +1,17 @@
+let data = new Date();
+console.log(data);
+
+let ano = data.getFullYear();
+console.log(ano);
+
+let mes = data.getMonth();
+console.log(mes);
+
+let dia = data.getDate();
+console.log(dia)
+
+let diasemana = data.getDay();
+console.log(dia)
+
+let hora = data.getHours();
+console.log(hora)
